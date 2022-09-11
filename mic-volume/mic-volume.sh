b51ec4f2b94801e8a1c6f196aec0b3d16d971e7b
jaroes <escobar.jarod@gmail.com>
2022-09-10 20:00:00 -0600

    Module will hide/show by reading the mic state

--- mic-volume/mic-volume.sh
+++ mic-volume/mic-volume.sh
@@ -8,7 +8,7 @@ display_volume() {
 	else
 	  volume="${volume//[[:blank:]]/}" 
 	  if [[ "$mute" == *"yes"* ]]; then
-	    echo "[$volume]"
+	    echo "$volume"
 	  elif [[ "$mute" == *"no"* ]]; then
 	    echo "$volume"
 	  else
@@ -17,17 +17,38 @@ display_volume() {
 	fi
 }
 
+volume() {
+	if [ -z "$1" ]; then
+  		volume=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 7 | grep "volume" | awk -F/ '{print $2}')
+  		mute=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 11 | grep "muted")
+		display_volume
+	else
+  		volume=$(pacmd list-sources | grep "$1" -A 6 | grep "volume" | awk -F/ '{print $2}')
+  		mute=$(pacmd list-sources | grep "$1" -A 11 | grep "muted" )
+		display_volume
+	fi
+}
+
+
+show_volume() {
+  STATUS=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 7 | grep "state" | awk -F: '{print $2}')
+  case $STATUS in
+    " SUSPENDED")
+      polybar-msg action "#mic-volume.module_hide"
+      ;;
+    " RUNNING")
+      polybar-msg action "#mic-volume.module_show" > /dev/null
+      volume "$1"
+      ;;
+    *)
+      polybar-msg action "#mic-volume.module_show" > /dev/null
+      ;;
+  esac
+}
+
 case $1 in
 	"show-vol")
-		if [ -z "$2" ]; then
-  			volume=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 7 | grep "volume" | awk -F/ '{print $2}')
-  			mute=$(pacmd list-sources | grep "index: $DEFAULT_SOURCE_INDEX" -A 11 | grep "muted")
-			display_volume
-		else
-  			volume=$(pacmd list-sources | grep "$2" -A 6 | grep "volume" | awk -F/ '{print $2}')
-  			mute=$(pacmd list-sources | grep "$2" -A 11 | grep "muted" )
-			display_volume
-		fi
+    show_volume "$2"
 		;;
 	"inc-vol")
 		if [ -z "$2" ]; then
